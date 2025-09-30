a7930cb05c5c1f9658c5ded57a7af043791bf26f
giahung140207 <gia.hung140207@gmail.com>
2025-09-30 15:53:33 +0700

    TS01072_NguyenGiaHung

--- src/main/java/poly/edu/controller/ProductController.java
+++ src/main/java/poly/edu/controller/ProductController.java
@@ -1,5 +1,7 @@
 package poly.edu.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,12 +9,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import poly.edu.model.Product;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class ProductController {
-
+    @Autowired
+    private HttpServletRequest request;
     @GetMapping("/product/form")
     public String form(Model model) {
         Product p = new Product();
@@ -20,7 +25,7 @@ public class ProductController {
         p.setPrice(5000.0);
         model.addAttribute("p1", p);
         model.addAttribute("p2", new Product());
-        model.addAttribute("items", getItems());
+        model.addAttribute("items", getItems);
         return "poly/product";
     }
 
@@ -28,14 +33,13 @@ public class ProductController {
     public String save(@ModelAttribute("p2") Product p, Model model) {
         Product defaultP = new Product("iPhone 30", 5000.0);
         model.addAttribute("p1", defaultP);
-
+        getItems.add(new Product(p.getName(), p.getPrice()));
         model.addAttribute("p2", p);
-        model.addAttribute("items", getItems());
+        model.addAttribute("items", getItems);
         return "poly/product";
     }
 
-    public List<Product> getItems() {
-        return Arrays.asList(new Product("A", 1.0), new Product("B", 12.0));
+    private List<Product> getItems = new ArrayList<>( Arrays.asList(new Product("A", 1.0), new Product("B", 12.0)));
     }
-}
+
 
